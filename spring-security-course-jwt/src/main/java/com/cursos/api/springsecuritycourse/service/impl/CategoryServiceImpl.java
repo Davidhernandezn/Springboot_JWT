59a515e84cf952c178b55f7c30354e1cf8855215
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-07-18 23:21:09 -0600

    Se modifico properties y se inserto informacion en la base de datos

--- spring-security-course-jwt/src/main/java/com/cursos/api/springsecuritycourse/service/impl/CategoryServiceImpl.java
+++ spring-security-course-jwt/src/main/java/com/cursos/api/springsecuritycourse/service/impl/CategoryServiceImpl.java
@@ -1,19 +1,18 @@
 package com.cursos.api.springsecuritycourse.service.impl;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import com.cursos.api.springsecuritycourse.dto.SaveCategory;
 import com.cursos.api.springsecuritycourse.exception.ObjectNotFoundException;
 import com.cursos.api.springsecuritycourse.persistence.entity.Category;
-import com.cursos.api.springsecuritycourse.persistence.entity.Product;
 import com.cursos.api.springsecuritycourse.persistence.repository.CategoryRepository;
 import com.cursos.api.springsecuritycourse.service.CategoryService;
 
+/*@Service: Indica que esta clase es un servicio de Spring, lo que significa que contiene 
+ * la lógica de negocio y se puede inyectar en otras partes de la aplicación.*/
 @Service
 public class CategoryServiceImpl implements CategoryService{
 	
@@ -22,19 +21,16 @@ public class CategoryServiceImpl implements CategoryService{
 
 	@Override
 	public Page<Category> findAll(Pageable pageable) {
-		// TODO Auto-generated method stub
 		return categoryRepository.findAll(pageable);
 	}
 
 	@Override
 	public Optional<Category> findOneById(Long productId) {
-		// TODO Auto-generated method stub
 		return categoryRepository.findById(productId);
 	}
 
 	@Override
 	public Category createOne(SaveCategory saveCategory) {
-		// TODO Auto-generated method stub
 		Category category = new Category();
 		category.setName(saveCategory.getName());
 		category.setStatus(Category.categoryStatus.ENABLED);
@@ -46,16 +42,17 @@ public class CategoryServiceImpl implements CategoryService{
 		Category categoryFromDB = categoryRepository.findById(categoryId)
 				.orElseThrow( ()-> new ObjectNotFoundException("category not found with"+categoryId)); //creamos exepcion propia
 		
-		categoryFromDB.setName(saveCategory.getName());
-	//	categoryFromDB.setStatus(Category.categoryStatus.ENABLED);
-		
+		categoryFromDB.setName(saveCategory.getName());		
 		return categoryRepository.save(categoryFromDB);
 	}
 
 	@Override
 	public Category disableOneById(Long categoryId) {
-		// TODO Auto-generated method stub
-		return null;
+		Category categoryFromDB = categoryRepository.findById(categoryId)
+				.orElseThrow( ()-> new ObjectNotFoundException("product not found with"+categoryId)); //creamos exepcion propia
+		
+		categoryFromDB.setStatus(Category.categoryStatus.DISABLED);
+		return categoryRepository.save(categoryFromDB);
 	}
 
 }
