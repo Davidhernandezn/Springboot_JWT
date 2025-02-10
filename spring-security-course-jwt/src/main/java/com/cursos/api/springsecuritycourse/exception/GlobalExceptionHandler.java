e3c652bc4866c18b022f6aa4872ff6228c16470d
Davidhernandezn <d4vidhernandezn@gmail.com>
2025-02-09 23:04:40 -0600

    Se agrega manejo de exepciones

---
+++ spring-security-course-jwt/src/main/java/com/cursos/api/springsecuritycourse/exception/GlobalExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.cursos.api.springsecuritycourse.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.cursos.api.springsecuritycourse.dto.ApiError;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	//RestController - CONTROLA PETICIONES HTTP
+	//@RestControllerAdvice MAPEA Y CONTROLA ERRORES EXEPCIONES
+	
+	//NOTACION PARA MANEJAR ERRORES, podemos pasar una excepcion o un arreglo
+	//podemos leer la documentacion de esta anotacion para ver lo que puede recibir en los parametros
+	
+	/**ERROR GENERICO*/
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handlerGenericException(HttpServletRequest request, Exception exception){
+		
+		ApiError apiError = new ApiError();
+		apiError.setBackendMessage(exception.getLocalizedMessage()); //INFORMATIVO QUE ERROR OCURRIO
+		apiError.setUrl(request.getRequestURL().toString()); //to string porque devuelve un objeto
+		apiError.setMethod(request.getMethod());
+		apiError.setMessage("Error interno en el servidor, vuelva a intentarlo");//
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+	}
+	
+	
+	
+	/**ERROR POR ARGUMENTOS NO VALIDOS*/
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> handlerMethodArgumentNotValidException(HttpServletRequest request, 
+			MethodArgumentNotValidException exception){
+		
+		ApiError apiError = new ApiError();
+		apiError.setBackendMessage(exception.getLocalizedMessage()); //INFORMATIVO QUE ERROR OCURRIO
+		apiError.setUrl(request.getRequestURL().toString()); //to string porque devuelve un objeto
+		apiError.setMethod(request.getMethod());
+		apiError.setMessage("Error en peticiòn enviada");//
+		
+		//MENSAJE ESPECIFICO
+		//INVESTIGAR
+		System.out.println(exception.getAllErrors().stream().map(each -> each.getDefaultMessage()));
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
+		//12.20
+	}
+}
