b02cc6915eea16cd05d8d028ca490a0f89ec4d5d
Davidhernandezn <d4vidhernandezn@gmail.com>
2025-04-21 08:06:03 -0600

    Se agrega metodo bean para provedor de identidad

--- spring-security-course-jwt/src/main/java/com/cursos/api/springsecuritycourse/config/security/SecurityBeansInjector.java
+++ spring-security-course-jwt/src/main/java/com/cursos/api/springsecuritycourse/config/security/SecurityBeansInjector.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 //PARA INYECTAR BEANS OCN LAS CLASE DEBES ANOTARLA CON CONFIGURATION
@@ -20,6 +22,17 @@ public class SecurityBeansInjector {
 		//EL METODO GENERA UNA EXEPCION POR LO QUE SE LE AGREGA THROWS
 	return	authenticationConfiguration.getAuthenticationManager(); //CREA AUTENTICACION (PROVIDE MANAGER), IMPLEMENTACION DEL AUTENTICATIO MANAGER
 		
+	}
+	
+	//PROVEEDOR DE AUTENTICACION
+	@Bean
+	public AuthenticationProvider autenticationProvider() {
+
+		//DaoAuthenticationProvider CON TIENE EL MANEJO DEL LOGIN
+		DaoAuthenticationProvider autenticationStrategy = new DaoAuthenticationProvider();
+		autenticationStrategy.setPasswordEncoder(null);
+		autenticationStrategy.setUserDetailsService(null);//estrategia de donde va a obtener al usuario en la base de datos
+		return autenticationStrategy;
 	}
 	
 
