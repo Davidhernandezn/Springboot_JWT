17f44872bfed9740c41363bfabcb8453bb331dc7
Davidhernandezn <d4vidhernandezn@gmail.com>
2025-04-21 23:43:06 -0600

    Creando UserDetailsService y PasswordEncoder

--- spring-security-course-jwt/src/main/java/com/cursos/api/springsecuritycourse/config/security/SecurityBeansInjector.java
+++ spring-security-course-jwt/src/main/java/com/cursos/api/springsecuritycourse/config/security/SecurityBeansInjector.java
@@ -7,6 +7,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.cursos.api.springsecuritycourse.exception.ObjectNotFoundException;
+import com.cursos.api.springsecuritycourse.persistence.repository.UserRepository;
 
 //PARA INYECTAR BEANS OCN LAS CLASE DEBES ANOTARLA CON CONFIGURATION
 @Configuration
@@ -14,6 +23,9 @@ public class SecurityBeansInjector {
 	
 	@Autowired
 	private AuthenticationConfiguration authenticationConfiguration; //DE SPRING sec
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	//PARA INYECTAR CUALQUIER TIPO DE BEAN, A  CONTENEDOR DE DEPENCENCIAS
 	@Bean
@@ -30,10 +42,34 @@ public class SecurityBeansInjector {
 
 		//DaoAuthenticationProvider CON TIENE EL MANEJO DEL LOGIN
 		DaoAuthenticationProvider autenticationStrategy = new DaoAuthenticationProvider();
-		autenticationStrategy.setPasswordEncoder(null);
-		autenticationStrategy.setUserDetailsService(null);//estrategia de donde va a obtener al usuario en la base de datos
+		autenticationStrategy.setPasswordEncoder(passwordEncoder());
+		autenticationStrategy.setUserDetailsService(userDetailsService());//estrategia de donde va a obtener al usuario en la base de datos (bd)
 		return autenticationStrategy;
 	}
 	
-
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+		
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+//		return new UserDetailsService() {
+//			/*AL AGREGARLO VA A SOBRESCRIBIR EL UNICO METODO QUE TIENE ES MEJOR AGREGARLO EN LAMBDA*/
+//			@Override
+//			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//				// TODO Auto-generated method stub
+//				return null;
+//			}
+//		};
+//		
+		/*Metodo que recibe un string username -> metodo a crear
+		 * */
+		return (username) -> {
+			return userRepository.findByUsername(username)//devuelve optional
+					.orElseThrow(() -> new ObjectNotFoundException("User not found with username " + username));
+		};
+	
+	}
 }
